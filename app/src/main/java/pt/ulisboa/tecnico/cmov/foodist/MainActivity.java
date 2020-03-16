17cd221a6439e87c6ec294b2b90ca8e4322af141
Glistener <diogomarante@tecnico.ulisboa.pt>
2020-03-16 16:44:41 +0000

    Forced commit
    had to commit because I had conflits when bringing changes to new branch

--- app/src/main/java/pt/ulisboa/tecnico/cmov/foodist/MainActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/foodist/MainActivity.java
@@ -3,6 +3,10 @@ package pt.ulisboa.tecnico.cmov.foodist;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        configureFoodListButton();
+
+        //get the spinner from the xml.
+        Spinner dropdown = findViewById(R.id.campus);
+        //create a list of items for the spinner.
+        String[] items = new String[]{"Alameda", "Taguspark"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
+    }
+
+    private void configureFoodListButton() {
+        Button foodListButton = (Button) findViewById(R.id.foodServicesButton);
+        foodListButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                startActivity(new Intent(MainActivity.this, ListFoodServices.class));
+            }
+        });
     }
 }
