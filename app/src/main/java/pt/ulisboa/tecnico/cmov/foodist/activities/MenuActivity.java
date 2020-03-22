c5b623cfd16b23ed8fa19d1660ce64859b1a4e1e
franciscomatos <francisco_matos98@hotmail.com>
2020-03-22 22:46:25 +0000

    tweaked menu interface

--- app/src/main/java/pt/ulisboa/tecnico/cmov/foodist/activities/MenuActivity.java
+++ app/src/main/java/pt/ulisboa/tecnico/cmov/foodist/activities/MenuActivity.java
@@ -3,11 +3,21 @@ package pt.ulisboa.tecnico.cmov.foodist.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.RectF;
+import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewParent;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -41,7 +51,15 @@ public class MenuActivity extends AppCompatActivity {
         Log.i("MenuActivity", "found Table");
 
         for(int i = 0; i < menuState.getCounter(); i += 2) {
+
             TableRow tr = (TableRow) getLayoutInflater().inflate(R.layout.activity_menu_dish, null);
+
+            ImageView imageViewLeft = tr.findViewById(R.id.menuDishPhotoLeft);
+            //roundImageCorners(imageViewLeft, true, true, false, false);
+
+            ImageView imageViewRight = tr.findViewById(R.id.menuDishPhotoRight);
+            //§roundImageCorners(imageViewRight, true, true, false, false);
+
 
             TextView dishNameLeftView = tr.findViewById(R.id.menuDishNameLeft);
             dishNameLeftView.setText(menuState.getDish(i).getName());
@@ -90,5 +108,78 @@ public class MenuActivity extends AppCompatActivity {
             this.tableLayout.addView(tr);
             Log.i("MenuActivity", "added row to table");
         }
+    }
+
+    public void roundImageCorners(ImageView image, boolean topLeft, boolean topRight,
+                                    boolean bottomLeft, boolean bottomRight) {
+        Bitmap mbitmap = ((BitmapDrawable) image.getDrawable()).getBitmap();
+        Bitmap imageRounded = Bitmap.createBitmap(175*14, 250*14, mbitmap.getConfig());
+        /*Bitmap imageRounded = Bitmap.createBitmap(mbitmap.getWidth(), mbitmap.getHeight(), mbitmap.getConfig());*/
+        Canvas canvas = new Canvas(imageRounded);
+        Paint mpaint = new Paint();
+        mpaint.setAntiAlias(true);
+        mpaint.setShader(new BitmapShader(mbitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP));
+
+        Path path = RoundedRect(0, 0,  175 *14f, 250*14f, 100, 100,
+                topLeft, topRight, bottomRight, bottomLeft);
+        /*Path path = RoundedRect(0, 0,  mbitmap.getWidth() , mbitmap.getHeight(), 100, 100,
+                topLeft, topRight, bottomRight, bottomLeft);*/
+
+        canvas.drawPath(path,mpaint);
+
+        image.setImageBitmap(imageRounded);
+
+    }
+
+    public static Path RoundedRect(
+            float left, float top, float right, float bottom, float rx, float ry,
+            boolean tl, boolean tr, boolean br, boolean bl
+    ){
+        Path path = new Path();
+        if (rx < 0) rx = 0;
+        if (ry < 0) ry = 0;
+        float width = right - left;
+        float height = bottom - top;
+        if (rx > width / 2) rx = width / 2;
+        if (ry > height / 2) ry = height / 2;
+        float widthMinusCorners = (width - (2 * rx));
+        float heightMinusCorners = (height - (2 * ry));
+
+        path.moveTo(right, top + ry);
+        if (tr)
+            path.rQuadTo(0, -ry, -rx, -ry);//top-right corner
+        else{
+            path.rLineTo(0, -ry);
+            path.rLineTo(-rx,0);
+        }
+        path.rLineTo(-widthMinusCorners, 0);
+        if (tl)
+            path.rQuadTo(-rx, 0, -rx, ry); //top-left corner
+        else{
+            path.rLineTo(-rx, 0);
+            path.rLineTo(0,ry);
+        }
+        path.rLineTo(0, heightMinusCorners);
+
+        if (bl)
+            path.rQuadTo(0, ry, rx, ry);//bottom-left corner
+        else{
+            path.rLineTo(0, ry);
+            path.rLineTo(rx,0);
+        }
+
+        path.rLineTo(widthMinusCorners, 0);
+        if (br)
+            path.rQuadTo(rx, 0, rx, -ry); //bottom-right corner
+        else{
+            path.rLineTo(rx,0);
+            path.rLineTo(0, -ry);
+        }
+
+        path.rLineTo(0, -heightMinusCorners);
+
+        path.close();//Given close, last lineto can be removed.
+
+        return path;
     }
 }
