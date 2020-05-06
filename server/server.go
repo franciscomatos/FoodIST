8804b208548ea0d1894094b5830950d13b5a4d4b
TChiclas <pedrombcustodio@gmail.com>
2020-05-06 16:53:26 +0100

    tls server side (dummy keys)

--- server/server.go
+++ server/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"crypto/tls"
 	"encoding/json"
 	"log"
 	"net/http"
@@ -835,9 +836,29 @@ func main() {
 	muxhttp.HandleFunc("/queue", queueHandler)
 	muxhttp.HandleFunc("/prefetch", prefetchMenuImages)
 
+	config_tls := &tls.Config{
+		MinVersion:               tls.VersionTLS12,
+		CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+		PreferServerCipherSuites: true,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+			tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+		},
+	}
+
+	server_http_tls := &http.Server{
+		Addr:         ":443",
+		Handler:      muxhttp,
+		TLSConfig:    config_tls,
+		TLSNextProto: make(map[string]func(*http.Server, *tls.Conn, http.Handler), 0),
+	}
+
 	go func() {
-		log.Println("Serving HTTP")
-		http.ListenAndServe(":8000", muxhttp)
+		log.Println("Serving TLS")
+		//log.fatal(server_http_tls.ListenAndServeTLS("../../ssl/server.crt", "../../ssl/server.key"))
+		server_http_tls.ListenAndServeTLS("ssl/server_tls.crt", "ssl/server_tls.key")
 	}()
 
 	<-finish
